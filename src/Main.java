fe79e89c550c1ace1649e9c712bab983edaaa1e6
Aditya Pant <pantanupam05@gmail.com>
2024-12-07 16:12:21 +0530

    first move

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(flourPacker(2,4,14));
     }
+    public static boolean flourPacker(int bigCount, int smallCount, int goal) {
+        int big= (bigCount*5);
+        int small=smallCount*1;
+        int totalCount = big+small;
+        if(totalCount == goal){
+            return true;
+        }
+
+        return false;
+    }
+
 }
